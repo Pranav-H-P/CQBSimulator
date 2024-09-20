764036019a89ee665f62e4ed8fca5732f08a758e
Pranav <pranavhp871@gmail.com>
2024-09-20 10:28:53 +0530

    Merged building mode script and building mode camera script

--- CQBSimulator/Scripts/buildingSetup.gd
+++ CQBSimulator/Scripts/buildingSetup.gd
@@ -6,14 +6,19 @@ extends Node3D
 @onready var statusBox=$CanvasLayer/Control/Status
 @onready var nextButton=$CanvasLayer/Control/Next
 @onready var inputBox=$CanvasLayer/Control/LineEdit
+@onready var camera=$Camera3D
+
+@export var camMoveSpeed=10
+
+var setMouse=false
 
 var currStage=0
 
 var mapData={
-	"floor":{
+	"foundation":{
 		
 	},
-	"walls":{
+	"innerWalls":{
 		
 	},
 	"doors":{
@@ -44,6 +49,42 @@ var stages={
 
 func _ready():
 	inputBox.visible=false
+
+func _input(event):
+	if event.is_action_pressed("leftClick"):
+		shoot_ray()
+	if event.is_action_pressed("rightClick"):
+		pass
+
+
+func _physics_process(delta):
+	var input_dir = Input.get_vector("a", "d", "w", "s")
+	
+	if Input.is_action_just_released("zoomIn"):
+		camera.global_position.y-=delta*camMoveSpeed*3
+	if Input.is_action_just_released("zoomOut"):
+		camera.global_position.y+=delta*camMoveSpeed*3
+	
+	
+	camera.global_position.x+=delta*camMoveSpeed*input_dir.x
+	camera.global_position.z+=delta*camMoveSpeed*input_dir.y
+	
+
+func shoot_ray():
+	var mouse_pos=camera.get_viewport().get_mouse_position()
+	var ray_length=1000
+	var from = camera.project_ray_origin(mouse_pos)
+	var to=from+camera.project_ray_normal(mouse_pos)*ray_length
+	
+	var space=camera.get_world_3d().direct_space_state
+	var ray_query=PhysicsRayQueryParameters3D.new()
+	ray_query.from=from
+	ray_query.to=to
+	var raycast_result=space.intersect_ray(ray_query)
+	
+	print(raycast_result)
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
